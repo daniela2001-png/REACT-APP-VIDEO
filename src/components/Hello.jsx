814537df0e9a8632c807426544f3c63a674aedaa
daniela2001-png <dmbmora@outlook.com>
2021-02-14 02:04:14 -0500

    adding files

---
+++ src/components/Hello.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../assets/styles/App.scss"
+
+export default function Hello() {
+    return (
+        <div>
+            <h1>Hello Mundo!</h1>
+        </div>
+    )
+}
